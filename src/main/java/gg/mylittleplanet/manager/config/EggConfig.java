9f65c98574346014ae2e41554c4058294da20e29
Tomáš "Wega" Weglarz <82312488+ThomasWega@users.noreply.github.com>
2026-03-03 23:38:45 +0100

    Revert "progress"
    This reverts commit 7de64903a455e2029afd436203f6b059fa7a6a6e.

--- src/main/java/gg/mylittleplanet/manager/config/EggConfig.java
+++ src/main/java/gg/mylittleplanet/manager/config/EggConfig.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class EggConfig {
-    private String file;           // path to egg JSON file
-    private String dockerImage;    // override docker image, optional
+    private int id;
+    private String dockerImage;
 }
